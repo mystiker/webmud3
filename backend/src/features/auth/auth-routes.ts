f4a79a69bd798d1ddd19e641de0161ac0118d63a
myst <anon@b.cd>
2024-05-31 06:30:13 +0200

    chore(backend): temp. disabled auth

--- backend/src/features/auth/auth-routes.ts
+++ backend/src/features/auth/auth-routes.ts
@@ -1,23 +1,23 @@
-import { Router } from 'express';
-import { AuthController } from './auth.js';
+// import { Router } from 'express';
+// import { AuthController } from './auth.js';
 
-const router = Router();
+// const router = Router();
 
-// middleware that is specific to this router
-router.use(function (req, res, next) {
-  const ip =
-    req.headers['x-forwarded-for'] ||
-    req.connection.remoteAddress ||
-    req.socket.remoteAddress ||
-    (req.socket ? req.socket.remoteAddress : null);
+// // middleware that is specific to this router
+// router.use(function (req, res, next) {
+//   const ip =
+//     req.headers['x-forwarded-for'] ||
+//     req.connection.remoteAddress ||
+//     req.socket.remoteAddress ||
+//     (req.socket ? req.socket.remoteAddress : null);
 
-  console.log(ip, '/api/auth', req.method, req.url);
+//   console.log(ip, '/api/auth', req.method, req.url);
 
-  next();
-});
+//   next();
+// });
 
-router.route('/login').post(AuthController.logon).get(AuthController.loggedon);
+// router.route('/login').post(AuthController.logon).get(AuthController.loggedon);
 
-router.route('/logout').post(AuthController.logout);
+// router.route('/logout').post(AuthController.logout);
 
-export default router;
+// export default router;
