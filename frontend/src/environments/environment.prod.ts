70217822fe30fd7529768cf3876a4e7d57f83688
myst <anon@b.cd>
2024-06-06 13:20:39 +0200

    feat(frontend): quickfix on prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  backendUrl: () => window.location.host,
+  backendUrl: () => window.location.origin,
 };
